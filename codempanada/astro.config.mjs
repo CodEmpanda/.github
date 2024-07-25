ed36120fcb0d6a181ede4a87864ebdbb76ca39cd
Eli-ezer Reuven Ramirez Ruiz <eramirez@uaf.gob.cl>
2024-07-25 12:53:25 -0400

    update configurations -> [update base, add redirects]

--- codempanada/astro.config.mjs
+++ codempanada/astro.config.mjs
@@ -3,6 +3,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://codempanada.github.io",
-    base: "/codempanada",
-    output: "static"
+    base: "/",
+    output: "static",
+    redirects:{
+        '/': "/.github",
+        '/codempanada': '/.github',
+        'other': {
+            status: 302,
+            destination: "/.github",
+        }
+    }
 });
