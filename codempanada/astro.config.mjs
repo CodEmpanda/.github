352562885fa620e92030c69190fa6738cd721f49
Eli-ezer Reuven Ramirez Ruiz <ramirez.ruiz.eliezer.reuven@gmail.com>
2024-07-29 00:16:21 -0400

    update publicDir, base

--- codempanada/astro.config.mjs
+++ codempanada/astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://codempanada.github.io",
-    base: "/codempanada",
+    base: "codempanada",
+    publicDir: "public",
     output: "static",
     trailingSlash: 'ignore',
     compressHTML: true,
